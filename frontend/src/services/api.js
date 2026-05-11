e21ab5b12049e418dff3d5ca3adcf4d11d7aadf7
Thanh Phuoc <thanhphuoc266.ntp@gmail.com>
2026-05-11 23:24:21 +0700

    Cap nhat link API cho frontend sang Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:1410/api",
+  baseURL: "https://collaborative-editor-zegd.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
