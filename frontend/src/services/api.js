b56e0247113336e3a85368fd01652c3b1bcddb34
Thanh Phuoc <thanhphuoc266.ntp@gmail.com>
2026-05-18 11:54:52 +0700

    fix api backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://collaborative-editor-zegd.onrender.com/api",
+  baseURL: "https://collaborative-editor-zegd.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
