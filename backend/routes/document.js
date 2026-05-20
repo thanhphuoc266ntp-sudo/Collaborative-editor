2561b0652495fa7676a637c38e3e2cab433a9328
Thanh Phuoc <thanhphuoc266.ntp@gmail.com>
2026-05-20 17:25:18 +0700

    Fix lỗi hoàn thành hệ thống

--- backend/routes/document.js
+++ backend/routes/document.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Document = require("../models/Document");
-const auth = require("../middleware/auth");
+const auth = require("../middleWare/authMiddleware");
 
 router.post("/", auth, async (req, res) => {
   try {
